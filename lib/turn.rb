dbf92f58e12e98215c42cce6635e3cdabe578ade
Connie Wang <conniewang2012@u.northwestern.edu>
2017-09-05 17:16:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def turn(board)
   idx = input_to_index(input)
 
   if valid_move?(board, idx)
-    move(board, idx, player)
+    board = move(board, idx, player)
     display_board(board)
   else
     turn(board)
