d0e395b56c236b90f3e56affa4df385c8d155331
Connie Wang <conniewang2012@u.northwestern.edu>
2017-09-05 17:15:15 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,11 +35,10 @@ def turn(board)
   input = gets.strip
   idx = input_to_index(input)
 
-  if valid_move(board, idx)
+  if valid_move?(board, idx)
     move(board, idx, player)
     display_board(board)
   else
-    puts "Please enter 1-9:"
     turn(board)
   end
 end
